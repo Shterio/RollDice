c1ff526a3ac6821ca9133eb1ef8e4d233402336d
Shterio <Shterio@users.noreply.github.com>
2019-10-24 17:39:27 +0200

    adding animation

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ export default class Die extends Component {
 	render() {
 		return (
 			<div>
-				<i className={`die fas fa-dice-${this.props.face}`} />
+				<i className={`die fas fa-dice-${this.props.face} ${this.props.rolling ? 'shaking' : ''}`} />
 			</div>
 		);
 	}
