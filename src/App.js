9a5baaf893d7e57cb776c3cae3666b30c8f6a207
Shterio <Shterio@users.noreply.github.com>
2019-10-25 12:31:49 +0200

    adding lottary test game

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './App.css';
 import RollDice from './RollDice';
+import Lottary from './Lottary';
 
 function App() {
 	return (
 		<div className='App'>
 			<RollDice />
+			<Lottary />
+			<Lottary title='Mini' numBalls={4} maxNum={10} />
 		</div>
 	);
 }
---
+++ src/Lottary.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Ball from './Ball';
+
+export default class Lottary extends Component {
+	static defaultProps = {
+		title: 'Lotto',
+		numBalls: 6,
+		maxNum: 40,
+	};
+	constructor(props) {
+		super(props);
+		this.state = { nums: Array.from({ length: this.props.numBalls }) };
+		this.handleClick = this.handleClick.bind(this);
+	}
+	generate() {
+		this.setState(curState => ({
+			nums: curState.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1),
+		}));
+	}
+
+	handleClick() {
+		this.generate();
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+				{this.state.nums.map(n => (
+					<Ball num={n} />
+				))}
+				<button onClick={this.handleClick}>GENERETE</button>
+			</div>
+		);
+	}
+}
